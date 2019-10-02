b1338405dfa4838001a0002ab4f69d6dd2496389
MilitaryJane <lyubovknyaginskaya@gmail.com>
2019-10-02 14:07:36 +0300

    intermediate changes

--- src/js/features-list_controls.js
+++ src/js/features-list_controls.js
@@ -3,28 +3,32 @@
     "use strict";
 
     var featuresList = document.querySelector('.features__list');
-    var button = document.querySelectorAll('.features__question');
-    button = Array.prototype.slice.call(button);
+    var buttons = document.querySelectorAll('.features__question');
+    buttons = Array.prototype.slice.call(buttons);
 
 
 
     featuresList.classList.remove('features__list--no-js');
 
 
-    button.forEach(function(element){
-
+    buttons.forEach(function(element){
       element.addEventListener('click', function (evt) {
         evt.preventDefault();
-        var buttonTick = evt.target.querySelector('.features__tick');
-        (buttonTick.classList.contains('features__tick--open') === true) ? buttonTick.classList
-            .remove(
-              'features__tick--open'): buttonTick.classList.add('features__tick--open');
+        var buttonTicks = evt.target.querySelectorAll('.features__tick');
+        buttonTicks = Array.prototype.slice.call(buttonTicks);
+        buttonTicks.forEach(function(element) {
+          (element.classList.contains('features__tick--open') === true) ? element.classList
+              .remove(
+                'features__tick--open'): element.classList.add('features__tick--open');
+          });
 
         var answerBlock = evt.target.parentNode.querySelector('.features__answer');
         (answerBlock.classList.contains('features__answer--open') === true) ? answerBlock
             .classList
             .remove(
                 'features__answer--open'): answerBlock.classList.add('features__answer--open');
+
+
     });
   })
 
