af28fdae6e125b4fc2f39d6766fdb664ae2ccf3b
MilitaryJane <lyubovknyaginskaya@gmail.com>
2019-10-03 16:25:59 +0300

    Create features-list controls

--- src/js/features-list_controls.js
+++ src/js/features-list_controls.js
@@ -1,35 +1,32 @@
 (function () {
 
-    "use strict";
+  "use strict";
 
-    var featuresList = document.querySelector('.features__list');
-    var buttons = document.querySelectorAll('.features__question');
-    buttons = Array.prototype.slice.call(buttons);
-
-
-
-    featuresList.classList.remove('features__list--no-js');
+  var featuresList = document.querySelector('.features__list');
+  var buttons = document.querySelectorAll('.features__question');
+  buttons = Array.prototype.slice.call(buttons);
+  var featuresItems = document.querySelectorAll('.features__item');
+  featuresItems = Array.prototype.slice.call(featuresItems);
 
 
-    buttons.forEach(function(element){
-      element.addEventListener('click', function (evt) {
-        evt.preventDefault();
-        var buttonTicks = evt.target.querySelectorAll('.features__tick');
-        buttonTicks = Array.prototype.slice.call(buttonTicks);
-        buttonTicks.forEach(function(element) {
-          (element.classList.contains('features__tick--open') === true) ? element.classList
-              .remove(
-                'features__tick--open'): element.classList.add('features__tick--open');
-          });
 
-        var answerBlock = evt.target.parentNode.querySelector('.features__answer');
-        (answerBlock.classList.contains('features__answer--open') === true) ? answerBlock
-            .classList
-            .remove(
-                'features__answer--open'): answerBlock.classList.add('features__answer--open');
+  featuresList.classList.remove('features__list--no-js');
 
 
+  buttons.forEach(function(element){
+    element.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      if (document.documentElement.clientWidth >= 765) {
+        featuresItems.forEach(function(element) {
+          element.classList.remove('features__item--open');
+        })
+      }
+
+      var answerBlock = evt.target.parentNode;
+      (answerBlock.classList.contains('features__item--open') === true) ? answerBlock
+      .classList
+      .remove('features__item--open'): answerBlock.classList.add('features__item--open');
     });
   })
-
 })();
