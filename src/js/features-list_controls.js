4cf41dfc29f2bfa89058e9f5f1a1ac706ecdb7ea
MilitaryJane <lyubovknyaginskaya@gmail.com>
2019-10-04 17:05:01 +0300

    Created control module of block "Features"

--- src/js/features-list_controls.js
+++ src/js/features-list_controls.js
@@ -1,7 +1,6 @@
 (function () {
 
   "use strict";
-
   var featuresList = document.querySelector('.features__list');
   var buttons = document.querySelectorAll('.features__question');
   buttons = Array.prototype.slice.call(buttons);
@@ -9,9 +8,10 @@
   featuresItems = Array.prototype.slice.call(featuresItems);
 
 
-
   featuresList.classList.remove('features__list--no-js');
 
+  var openElem = document.querySelector('.features__item--open').querySelector('.features__answer');
+  window.checkHeight(featuresList, openElem);
 
   buttons.forEach(function(element){
     element.addEventListener('click', function (evt) {
@@ -23,10 +23,14 @@
         })
       }
 
-      var answerBlock = evt.target.parentNode;
-      (answerBlock.classList.contains('features__item--open') === true) ? answerBlock
+      var featuresItemShow = evt.target.parentNode;
+
+      (featuresItemShow.classList.contains('features__item--open') === true) ? featuresItemShow
       .classList
-      .remove('features__item--open'): answerBlock.classList.add('features__item--open');
+      .remove('features__item--open') : featuresItemShow.classList.add('features__item--open');
+
+      window.checkHeight(featuresList, featuresItemShow.querySelector('.features__answer'));
+
     });
   })
 })();
