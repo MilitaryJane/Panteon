06b0acf5ee55ed90d0d45819ee3fb021726c15fc
MilitaryJane <lyubovknyaginskaya@gmail.com>
2019-10-17 13:00:19 +0300

    Add: video is worked, if no js

--- src/js/video_controls.js
+++ src/js/video_controls.js
@@ -3,6 +3,13 @@
 
   var playButtons = document.querySelectorAll('.video__play-button');
   playButtons = Array.prototype.slice.call(playButtons);
+  var videos = document.querySelectorAll('.video');
+  videos = Array.prototype.slice.call(videos);
+
+  videos.forEach(function(element){
+    element.classList.remove('video--no-js');
+  })
+
 
   playButtons.forEach(function(element) {
     element.addEventListener('click', function(evt) {
